ecc7487e3bebf9e7a912bbdd34316d5ef40976ed
Wang Wei <thegenius@vip.qq.com>
2019-03-24 13:07:15 +0800

    add gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rocksdb::{DB, Options};
+
 fn main() {
+
+// NB: db is automatically closed at end of lifetime
+    let path = "rocks_data";
+    {
+        let db = DB::open_default(path).unwrap();
+        db.put(b"my key", b"my value").unwrap();
+        match db.get(b"my key") {
+            Ok(Some(value)) => println!("retrieved value {}", value.to_utf8().unwrap()),
+            Ok(None) => println!("value not found"),
+            Err(e) => println!("operational problem encountered: {}", e),
+        }
+        db.delete(b"my key").unwrap();
+    }
+    let _ = DB::destroy(&Options::default(), path);
     println!("Hello, world!");
 }
